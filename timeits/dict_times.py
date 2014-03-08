d28159084dc7f51e4759f994eb26127b2acfba9b
Konstantin Martini <k@tuxcode.org>
2014-03-08 01:59:57 +0100

    add ite-get

--- timeits/dict_times.py
+++ timeits/dict_times.py
@@ -1,27 +1,54 @@
 from collections import namedtuple
 
-SpeedTest = namedtuple("SpeedTest", ["title", "test", "setup"])
+SpeedTest = namedtuple("SpeedTest", ["title", "setup", "code"])
 
 tests = [
-SpeedTest("access 1-elem dict", "a[1]", "a={1:'x'}"),
-SpeedTest("get from empty dict", "x=a.get(1,'x')", "a={}"),
-SpeedTest("get from 1-elem dict", "x=a.get(1,'x')", "a={1:'x'}"),
-SpeedTest("try-except to emulate get (raw)",
+SpeedTest("access 1-elem dict", "a={1:'x'}", "a[1]"),
+SpeedTest("get from empty dict", "a={}", "x=a.get(1,'x')"),
+SpeedTest("get from 1-elem dict", "a={1:'x'}", "x=a.get(1,'x')"),
+SpeedTest("if-then-else-get (in dict)",
+"a={1: 'x'}",
+"""
+if 1 in a:
+    x = a[1]
+else:
+    x = "x"
+""",
+),
+SpeedTest("if-then-else-get (not in dict)",
+"a={}",
+"""
+if 1 in a:
+    x = a[1]
+else:
+    x = "x"
+""",
+),
+SpeedTest("try-except working get (raw)",
+"a={1:'x'}",
 """
 try:
     x = a[1]
 except:
     x = "x"
 """,
-    "a={}"
+),
+SpeedTest("try-except to emulate get (raw)",
+"a={}",
+"""
+try:
+    x = a[1]
+except:
+    x = "x"
+""",
 ),
 SpeedTest("try-except to emulate get (specialized)",
+"a={}",
 """
 try:
     x = a[1]
 except KeyError:
     x = "x"
 """,
-    "a={}"
 ),
 ]
--- timeits/runtimes.py
+++ timeits/runtimes.py
@@ -7,7 +7,7 @@ Usage:
 Options:
     -h, --help      Show this help.
     -r, --run       Run tests. (default)
-    -l, --list      List available tests.
+    -l, --list      List available tests. (implied if no other action)
     -s, --show      Show full tests.
     -c, --count=N   Run each test N times. [default: 1000000]
     -v, --verbose   Run and show tests.
@@ -18,46 +18,50 @@ Arguments:
 from docopt import docopt
 args = docopt(__doc__)
 
-if args['--verbose']:
+if args['--verbose'] or args['NUMBER']:
     args['--run'] = True
-    #print args
+elif not args['--run']:
+    args['--list'] = True
 
-if args['NUMBER'] is None:
-    args['NUMBER'] = "all"
 
 from collections import namedtuple
 
 from dict_times import tests
-
-print "starting up..."
 
 if args['--list']:
     for i, test in enumerate(tests):
         print "%3d: %s" % (i, test.title)
 
 def run_full(i, test):
-    print "Test number %d - %s" % (i, test.title)
+    print "Test number %d : %s" % (i, test.title)
     print "Setup:"
     print test.setup
     print "Code:"
-    print test.test
+    print test.code
     print "Result:"
-    print timeit(test.test, test.setup, number=NUMTST)
+    print timeit(test.code, test.setup, number=NUMTST)
     print
 
 def run_short(i, test):
     print "No. %d: %s ... " % (i, test.title)
-    print timeit(test.test, test.setup, number=NUMTST)
+    print timeit(test.code, test.setup, number=NUMTST)
     print
 
 if args['--run']:
     from timeit import timeit
     NUMTST=int(args['--count'])
-    if args['--verbose']:
-        for i, test in enumerate(tests):
-            run_full(i, test)
-    else:
-        for i, test in enumerate(tests):
-            run_short(i, test)
 
-print 'done.'
+    if args['--verbose']:
+        run = run_full
+    else:
+        run = run_short
+
+    if not args['NUMBER']:
+        for i, test in enumerate(tests):
+            run(i, test)
+    else:
+        nums = map(int, args['NUMBER'])
+        for num in nums:
+            run(num, tests[num])
+
+    print 'done.'
